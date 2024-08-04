7bfa06cfff80ff14579cff16f48545e09bdf5913
Ojukwu-Valentine1933 <thejasperval@gmail.com>
2024-08-04 01:42:23 +0100

    added serviceWorkerRegistration file

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import Layout from "./component/layout/Layout";
-import "./App.css"
+import "./App.css";
 
 function App() {
   return (
-   <Layout/>
+    <>
+      <Layout />
+      <div className="App">
+        <header className="App-header">
+          <h1>Sona AI</h1>
+          <button id="install-btn">Install App</button>
+        </header>
+      </div>
+    </>
   );
 }
 
